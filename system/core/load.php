153d321dcf9ffbb4cd9fe803852ce7a6336ba63b
Joe <e@enozoom.com>
2015-10-08 14:22:33 +0800

    移除Mthaml,为library()增加对libraries中存在文件夹同名的基类的自动载入

--- system/core/load.php
+++ system/core/load.php
@@ -1,55 +1,62 @@
-<?php //use MtHaml\Autoloader;
-defined('APPPATH') OR exit('POWERED BY Enozoomstudio');
+<?php defined('APPPATH') OR exit('POWERED BY Enozoomstudio');
 require SYSPATH.'core/model.php';
 /**
  * 载入model,library,helper,view
  * @author Joe
+ * ------------------------------
+ * 2015年10月8日14:09:19
+ * 增加对libraries中文件夹中有同名基类的支持
+ * 如libraries/a/a.php,libraries/a/b.php
+ * 其中b extends a
+ * 在controller引用时，只需要引用b,如 $controller->load->library('a/b')
+ * 则会自动require libraries/a/a.php
  */
-class ES_load{	
-	private $dir_model	 = 'models/';
-	private $dir_view 	 = 'views/';
-	private $dir_helper  = 'helpers/';
-	private $dir_library = 'libraries/';
+class ES_load{  
+  private $dir_model   = 'models/';
+  private $dir_view    = 'views/';
+  private $dir_helper  = 'helpers/';
+  private $dir_library = 'libraries/';
 
 /**
  * 获取配置参数
  * @param string $file 配置文件的文件名
  * @param string $k 需要获取$k的值
- */	
-	public function config($file='config',$k=''){
-		global $configs;
-		if(empty($k)) return $configs->$file;
-		return isset($configs->$file->$k)? $configs->$file->$k: false;
-	}
-	
+ */  
+  public function config($file='config',$k=''){
+    global $configs;
+    if(empty($k)) return $configs->$file;
+    return isset($configs->$file->$k)? $configs->$file->$k: false;
+  }
+  
 /**
 * 载入类库
 * @param string $cls   库类
 * @param string $alias 别名
 * @return ES_load
-*/	
-	public function library($cls,$alias=FALSE){
-		$controller = ES_controller::get_instance();
-		
-		$file = APPPATH.$this->dir_library.$cls.'.php';
-		if(!is_file($file)){ // 如果APPPATH中没有，则去SYSPATH中查找
-			$file = SYSPATH.$this->dir_library.$cls.'.php';
-		}
-		
-		is_file($file) || show_500('library不存在，'.$file);
-		
-		require($file);
-		
-		if( ($idx = strpos($cls,'/')) !== FALSE  ){
-			$cls = substr($cls,$idx+1);
-		}
-		
-		empty($alias) && $alias = $cls;
-		$cls = ucfirst($cls);
-		$_cls = new $cls();
-		$controller->$alias = &$_cls;
-		return $this;
-	}
+*/  
+  public function library($cls,$alias=FALSE){
+    $controller = ES_controller::get_instance();
+    $file = APPPATH.$this->dir_library.$cls.'.php';
+    if(!is_file($file)){ // 如果APPPATH中没有，则去SYSPATH中查找
+      $file = SYSPATH.$this->dir_library.$cls.'.php';
+    }
+    is_file($file) || show_500('library不存在，'.$file);
+    
+    if( ($idx = strpos($cls,'/')) !== FALSE  ){
+      $parent_cls = dirname($file).'/'.substr($cls, 0,$idx).'.php';
+      if( file_exists($parent_cls) ){
+        require $parent_cls;
+      }
+      $cls = substr($cls,$idx+1);
+    }
+    
+    require $file;
+    empty($alias) && $alias = $cls;
+    $cls = ucfirst($cls);
+    $_cls = new $cls();
+    $controller->$alias = &$_cls;
+    return $this;
+  }
 
 /**
 * 载入model
@@ -57,85 +64,63 @@ class ES_load{
 * @param string $alias 别称
 * 
 * @return ES_load
-*/	
-	public function model($cls,$alias=FALSE){
-		$controller = ES_controller::get_instance();
-		$file = APPPATH.$this->dir_model.$cls.'.php';
-		is_file($file) || show_500('model不存在，'.$file);
-		require($file);
-		empty($alias) && $alias = $cls;
-		$cls = ucfirst($cls);
-		$_model = new $cls();
-		$_model instanceof ES_model || show_500($cls.'非ES_model子类');
-		$controller->$alias = &$_model;
-		return $this;
-	}
+*/  
+  public function model($cls,$alias=FALSE){
+    $controller = ES_controller::get_instance();
+    $file = APPPATH.$this->dir_model.$cls.'.php';
+    is_file($file) || show_500('model不存在，'.$file);
+    require($file);
+    empty($alias) && $alias = $cls;
+    $cls = ucfirst($cls);
+    $_model = new $cls();
+    $_model instanceof ES_model || show_500($cls.'非ES_model子类');
+    $controller->$alias = &$_model;
+    return $this;
+  }
 /**
 * 载入helper函数包
 * @param string $helper 如果导入多个helper，用 “,” 分割
 * 
 * @return ES_load
-*/	
-	public function helper($helper){
-		$helpers = array();
-		if( is_array($helper) ){
-			$helpers = $helper;
-		}else{
-			if( is_string($helper) && strpos($helper,',') ){
-				 $helpers = explode(',',$helper);
-			}else{
-				$helpers[] = $helper;
-			}
-		}
-		// Fixed 2015-04-21 22:14:24  修正不能同时载入SYSPATH,APPPATH下的同名文件
-		foreach($helpers as $helper){
-			$file = APPPATH.$this->dir_helper.$helper.'_helper.php';
-			is_file($file) && require($file);
-			$file = SYSPATH.$this->dir_helper.$helper.'_helper.php';
-			is_file($file) && require($file);
-		}
-		return $this;
-	}
-	
+*/  
+  public function helper($helper){
+    $helpers = array();
+    if( is_array($helper) ){
+      $helpers = $helper;
+    }else{
+      if( is_string($helper) && strpos($helper,',') ){
+         $helpers = explode(',',$helper);
+      }else{
+        $helpers[] = $helper;
+      }
+    }
+    // Fixed 2015-04-21 22:14:24  修正不能同时载入SYSPATH,APPPATH下的同名文件
+    foreach($helpers as $helper){
+      $file = APPPATH.$this->dir_helper.$helper.'_helper.php';
+      is_file($file) && require($file);
+      $file = SYSPATH.$this->dir_helper.$helper.'_helper.php';
+      is_file($file) && require($file);
+    }
+    return $this;
+  }
+  
 /**
 * 载入视图
 * @param string $dir html/php视图的文件路径，application/views下的什么位置
 * @param array $data
 * 
 * @return void
-*/	
-	public function view($dir,$data=array()){
-		!empty($data) && is_array($data) && extract($data);
-		$file = APPPATH.$this->dir_view.'html/'.$dir.'.php';
-		is_file($file) || show_500('view文件不存在,'.$file);
-		ob_start();
-		include($file);
-		$output = ob_get_contents();
-		@ob_end_clean();
-		$controller = ES_controller::get_instance();
-		$controller->output->append_output($output);
-	}
-/**
- * 解析haml
- * @param string $dir haml视图的文件路径，application/views下的什么位置
- * @param array $data
- */
-	public function haml($dir,$data=array()){
-/*
-	    $dir_cache = APPPATH.'cache';
-		$file = APPPATH.$this->dir_view.'haml/'.$dir.'.haml';
-		
-		require SYSPATH.'libraries/MtHaml/Autoloader.php';
-		MtHaml\Autoloader::register();
-		
-		$haml = new MtHaml\Environment('php');
-		$executor = new MtHaml\Support\Php\Executor($haml, array('cache' => $dir_cache.'/haml'));
-		$output = $executor->render($file,$data);
-		$controller = ES_controller::get_instance();
-		$controller->output->append_output($output);
-*/
-	}
-	
+*/  
+  public function view($dir,$data=array()){
+    !empty($data) && is_array($data) && extract($data);
+    $file = APPPATH.$this->dir_view.'html/'.$dir.'.php';
+    is_file($file) || show_500('view文件不存在,'.$file);
+    ob_start();
+    include($file);
+    $output = ob_get_contents();
+    @ob_end_clean();
+    $controller = ES_controller::get_instance();
+    $controller->output->append_output($output);
+  }
 
 }
-?>
