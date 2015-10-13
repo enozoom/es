581ae8f5f395033f5c9eb4c0e37569eca3bdfb44
Joe <e@enozoom.com>
2015-10-13 08:58:51 +0800

    数组转XML

---
+++ system/libraries/api_wechat/helper_arr2xml.php
@@ -0,0 +1,58 @@
+<?php defined('APPPATH') OR exit('POWERED BY Enozoomstudio');
+class SimpleXMLExtended extends SimpleXMLElement{
+	public function addCData($cdata_text){
+		if(!is_numeric($cdata_text)){
+			$node = dom_import_simplexml($this);
+			$no   = $node->ownerDocument;
+			$node->appendChild($no->createCDATASection($cdata_text));			
+		}
+		
+	}
+}
+
+//header('Content-Type: text/xml');
+/**
+* 数组转换成XML
+* 深度节点二层
+*/
+class Arr2xml{
+	
+/**
+* 增加节点值
+* @param array $data
+* @param SimpleXMLElement $xml
+* 
+* @return void
+*/	
+	public static function node($data,SimpleXMLExtended &$xml){
+		foreach($data as $k=>$v){
+			if(is_array($v)){
+				is_numeric($k) && $k = substr($xml->getName(),0,-1);
+				$subroot = $xml->addChild($k);
+				Arr2xml::node($v,$subroot);
+			}else{
+				if(is_numeric($v)){
+					$xml->addChild($k,$v);
+				}else{
+					$xml->addChild($k)->addCData($v);
+				}
+			}
+		}
+	}
+	
+/**
+* 直接将数组转化为xml
+* @param array $data	需要被转的数组
+* @param string $root 数组的根标签
+* @param bool $return_header 要不要含声明一起返回
+* @return
+*/	
+	public static function toXml($data, $root = 'xml',$return_header=FALSE){
+		$xml = new SimpleXMLExtended("<?xml version='1.0' encoding='utf-8'?><{$root} />");
+		Arr2xml::node($data,$xml);
+		$xmlstr = $xml->asXML();
+		$xmlstr = preg_replace('@<\?.*?>@','',$xmlstr);
+		return trim($xmlstr);
+    }
+}
+?>
