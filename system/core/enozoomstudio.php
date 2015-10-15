35e49cab133ba687b6a66e8cd0bea4463afbfc4f
Joe <e@enozoom.com>
2015-10-15 15:33:46 +0800

    版本升级到2.1151013，增强载入，增加缓存

--- system/core/enozoomstudio.php
+++ system/core/enozoomstudio.php
@@ -16,25 +16,30 @@ defined('SYSPATH') OR exit('POWERED BY Enozoomstudio');
  * ------------
  * V2.1150624
  * 增加：namespace，less，haml的解析支持，让框架更关注业务逻辑，不再关注前端
+ * ------------
+ * V2.1151013
+ * 并统一部分命名
+ * 增加对缓存的支持
+ * 增加微信的开发包system/libraries/api_wechat
  */
 
 // 计时器
 $_es_timer_begin = microtime(1);
 
 // 版本
-define('ES_VERSION','2.1151002');
+define('ES_VERSION','2.1151013');
 // 通用函数库 
 require 'common.php';
 
 // 自动导入类
 spl_autoload_register('auto_loader');
 // 配置文件
-$configs = Config::init();
+$configs = ES_config::init();
 //是否开启调试
 error_reporting($configs->config->debug?E_ALL:0);
 // 核心路由
-$Route = new Route($configs->routes);
-
+$Route = new ES_route($configs->routes);
+// 开始初始化控制器
 $Route->resolve();
 // 页面执行时间
 if($configs->config->timer) echo PHP_EOL.'runtime:'.(microtime(1)-$_es_timer_begin).'s';
