97f14ca61133a5ff973232d64ddd0f641a27bf3f
Joe <e@enozoom.com>
2015-12-19 14:16:05 +0800

    增加获取控制器文件夹，控制名，控制器方法名，控制器方法参数的 protected function _cmdq()

--- system/core/controller.php
+++ system/core/controller.php
@@ -1,4 +1,13 @@
 <?php defined('SYSPATH') OR exit('POWERED BY Enozoomstudio');
+/**
+ * 基类控制器
+ * @author Joe e@enozoom.com
+ * Powered by Enozoomstudio
+ * 2015年12月19日下午2:08:37
+ * ------------------------------
+ * 更改 _is_post()修饰符protected
+ * 增加 cmdq() 获取当前控制器的文件夹，控制器名，控制器方法，控制器方法参数
+ */
 class ES_controller{
   private static $instance;
   public function __construct(){
@@ -29,7 +38,7 @@ class ES_controller{
    * 是否为POST提交,所有的数据修改均需要POST提交.
    * @return bool
    */
-  public function _is_post(){
+  protected function _is_post(){
     return strtolower($_SERVER['REQUEST_METHOD']) == 'post';
   }
   /**
@@ -37,5 +46,10 @@ class ES_controller{
    */
   public function index(){
     echo 'Hi,ES!';
+  }
+  
+  protected function _cmdq(){
+    global $Route;
+    return $Route->cmdq;
   }
 }
