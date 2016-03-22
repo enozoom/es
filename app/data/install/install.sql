f08f7212b5bbe34f1449bfff7114cdd93e22ddfa
Joe <e@enozoom.com>
2016-03-22 13:43:27 +0800

    增加category_sequence字段

--- app/data/install/install.sql
+++ app/data/install/install.sql
@@ -1,3 +1,6 @@
+# 使用数据库
+USE brsys;
+
 # A文章
 DROP TABLE IF EXISTS `es_article`;
 CREATE TABLE `es_article` (
@@ -37,6 +40,7 @@ CREATE TABLE `es_category` (
   `category_name` varchar(20) NOT NULL DEFAULT '',
   `category_pid` smallint unsigned NOT NULL DEFAULT 0,
   `category_intro` varchar(200) NOT NULL DEFAULT '',
+  `category_sequence` smallint unsigned NOT NULL DEFAULT 0,
   KEY(`category_pid`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -58,7 +62,28 @@ INSERT INTO `es_category` VALUES
 (41,'状态',4,''),
 (411,'正常',41,'前台显示'),
 (412,'禁用',41,'不显示，限制登录'),
-(413,'回收',41,'仅后台显示');
+(413,'回收',41,'仅后台显示'),
+(5,'链接',0,''),
+(51,'广告链',5,''),
+(52,'友情链',5,''),
+(6,'大类',0,''),
+(7,'子类',0,''),
+(8,'订单',0,'');
+
+
+
+# L链接广告
+DROP TABLE IF EXISTS `es_link`;
+CREATE TABLE `es_link`(
+  `link_id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `link_word` varchar(90) NOT NULL DEFAULT '',
+  `pic_id` varchar(50) NOT NULL DEFAULT '',
+  `link_target` varchar(200) NOT NULL DEFAULT '#',
+  `status_id` smallint unsigned NOT NULL DEFAULT 411,
+  `category_id` smallint unsigned NOT NULL DEFAULT 51,
+  `link_sequence` int unsigned NOT NULL DEFAULT 0,
+   KEY (`category_id`)
+);
 
 
 # O拓展参数键
@@ -82,6 +107,30 @@ CREATE TABLE `es_option_value`(
   `ov_value` varchar(300) NOT NULL DEFAULT '',
   UNIQUE KEY (`fkid_id`,`ok_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+# O订单
+DROP TABLE IF EXISTS `es_order`;
+CREATE TABLE `es_order`(
+  order_id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  order_timestamp int unsigned NOT NULL DEFAULT 0,
+  order_no varchar(25) NOT NULL DEFAULT '' UNIQUE,
+  order_reservetime int unsigned NOT NULL DEFAULT 0,
+  brand_id int unsigned NOT NULL DEFAULT 0,
+  subb_id int unsigned NOT NULL DEFAULT 0,
+  broker_id int unsigned NOT NULL DEFAULT 0,
+  customer_name varchar(10) NOT NULL DEFAULT '',
+  customer_mobile char(11) NOT NULL DEFAULT 0,
+  status_id smallint unsigned NOT NULL DEFAULT 0,
+  category_id smallint unsigned NOT NULL DEFAULT 0,
+  order_etc varchar(300) NOT NULL DEFAULT '',
+  order_amount decimal(8,2) NOT NULL DEFUALT 0,
+  KEY(customer_mobile),
+  KEY(status_id),
+  KEY(broker_id,category_id,status_id),
+  KEY(category_id,status_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 # P图片
 DROP TABLE IF EXISTS `es_pic`;
@@ -132,6 +181,7 @@ CREATE TABLE `es_usr` (
   `level_id` smallint unsigned NOT NULL DEFAULT 121,
   `category_id` smallint unsigned NOT NULL DEFAULT 113,
   `ok_ids` varchar(50) NOT NULL DEFAULT '',
-  KEY (`usr_mobile`,`usr_pword`,`category_id`,`status_id`),
+  `fk_usr_id` int unsigned NOT NULL DEFAULT 0,
+  KEY (`usr_mobile`,`status_id`,`usr_pword`,`category_id`),
   KEY (`usr_name`,`usr_pword`,`category_id`,`status_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
