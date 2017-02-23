cd3febcda7469e54e6e6514c16ab2c100f12269e
Joe <e@enozoom.com>
2017-02-23 14:06:03 +0800

    放开对PHP7的支持

--- es/core/Hook/SystemHook.php
+++ es/core/Hook/SystemHook.php
@@ -7,8 +7,8 @@ class SystemHook extends HookAbstract{
   public function beforeController()
   {
     // 版本检查
-    if(!(version_compare(PHP_VERSION, '5.5.0', '>') && version_compare(PHP_VERSION, '7.0.0', '<'))){
-      die('Requires PHP version between 5.5.x and 5.6.x, Your version: ' . PHP_VERSION );
+    if(!(version_compare(PHP_VERSION, '5.5.0', '>') )){
+      die('Requires PHP version >= 5.5.x, Your version: ' . PHP_VERSION );
     }
     // 防御XSS攻击
     \es\core\Toolkit\XssStaitc::defense();
